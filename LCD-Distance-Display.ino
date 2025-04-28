b517024530b3a0e77ee07d5b2df5caa8637e1f09
LamaV10 <lama.customs@gmail.com>
2025-04-28 16:47:21 +0200

    added a delay for the distance so its easier to read and got rid of lcd.clear. Now only the second column is getting overwritten with ' ' if the number is only one column long.

--- LCD-Distance-Display.ino
+++ LCD-Distance-Display.ino
@@ -4,6 +4,7 @@
 
 int duration;
 int distance;
+int printDelay;
 
 // Create an LCD object. Parameters: (RS, E, D4, D5, D6, D7):
 LiquidCrystal lcd = LiquidCrystal(2, 3, 4, 5, 6, 7);
@@ -29,20 +30,31 @@ void loop() {
 
  
   if(distance > 0){
-    lcd.clear();
-
     lcd.home(); // go to 0, 0
     lcd.print("Distance: ");
     lcd.setCursor(12, 0);
     lcd.print("cm"); // print the unit of the distance 
 
-    lcd.setCursor(10, 0);
-    lcd.print(distance); // print the distance
+    // deletes the second column, if the number is only one column long
+    if(distance < 10){
+      lcd.setCursor(11, 0);
+      lcd.print(" ");
+    }
+    // a little delay for printing otherwise it's hardly readable
+    if(printDelay >= 10){
+      lcd.setCursor(10, 0);
+      lcd.print(distance); // print the distance
+      printDelay = 0;
+    } else {
+      printDelay++;
+    }
 
+    // printing the distance symbols
     lcd.setCursor(0, 1);
     lcd.print(">>>>>>>>>>>>>>>>"); // write the whole row full with "-" 
     lcd.setCursor(15, 1);
 
+    // delete the symbols according to the distance
     for(int i = 0; i < distance; i += 2){
       lcd.print(" "); // delete the "-" string from the right
       lcd.setCursor(15 - i / 2, 1); // positions cursor one to the left for the next potential deletion
